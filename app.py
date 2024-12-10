ab1b68f804a16204fb5535c833075f5562501b12
rakesh 09 <rakeshmr9999@gmail.com>
2024-12-10 08:44:48 +0530

    final commit

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ scaler= pickle.load(open('scaling.pkl','rb'))
 def home():
     return render_template('home.html')
 
+
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
     data= request.json['data']
@@ -21,6 +22,15 @@ def predict_api():
     output = reg_model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data= [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = reg_model.predict(final_input)[0]
+    return render_template("home.html", prediction_text = "the house price is {}".format(output))
 
 
 if __name__ == "__main__":
